498502f6376cc695a7eb923f62b19c1f2d638823
saiprasad1586 <saiprasadgovindu@gmail.com>
2020-10-30 15:22:21 +0530

    Initial Commit

---
+++ app.py
@@ -0,0 +1,125 @@
+import time
+from flask import Flask
+from flask import render_template
+from flask import request
+import numpy as np
+import os
+import sys
+import tensorflow as tf
+from distutils.version import StrictVersion
+from matplotlib import pyplot as plt
+from PIL import Image
+from object_detection.utils import ops as utils_ops
+from object_detection.utils import label_map_util
+from object_detection.utils import visualization_utils as vis_util
+
+sys.path.append("..")
+
+
+if StrictVersion(tf.__version__) < StrictVersion('1.9.0'):
+  raise ImportError('Please upgrade your TensorFlow installation to v1.9.* or later!')
+
+
+
+MODEL_NAME = 'inference_graph'
+PATH_TO_FROZEN_GRAPH = MODEL_NAME + '/frozen_inference_graph.pb'
+PATH_TO_LABELS = os.path.join(MODEL_NAME, 'labelmap.pbtxt')
+detection_graph = tf.Graph()
+with detection_graph.as_default():
+  od_graph_def = tf.GraphDef()
+  with tf.gfile.GFile(PATH_TO_FROZEN_GRAPH, 'rb') as fid:
+    serialized_graph = fid.read()
+    od_graph_def.ParseFromString(serialized_graph)
+    tf.import_graph_def(od_graph_def, name='')
+
+category_index = label_map_util.create_category_index_from_labelmap(PATH_TO_LABELS, use_display_name=True)
+
+def load_image_into_numpy_array(image):
+  (im_width, im_height) = image.size
+  return np.array(image.getdata()).reshape(
+      (im_height, im_width, 3)).astype(np.uint8)
+
+def run_inference_for_single_image(image, graph):
+  with graph.as_default():
+    with tf.Session() as sess:
+      ops = tf.get_default_graph().get_operations()
+      all_tensor_names = {output.name for op in ops for output in op.outputs}
+      tensor_dict = {}
+      for key in [
+          'num_detections', 'detection_boxes', 'detection_scores',
+          'detection_classes', 'detection_masks'
+      ]:
+        tensor_name = key + ':0'
+        if tensor_name in all_tensor_names:
+          tensor_dict[key] = tf.get_default_graph().get_tensor_by_name(
+              tensor_name)
+      if 'detection_masks' in tensor_dict:
+        detection_boxes = tf.squeeze(tensor_dict['detection_boxes'], [0])
+        detection_masks = tf.squeeze(tensor_dict['detection_masks'], [0])
+        real_num_detection = tf.cast(tensor_dict['num_detections'][0], tf.int32)
+        detection_boxes = tf.slice(detection_boxes, [0, 0], [real_num_detection, -1])
+        detection_masks = tf.slice(detection_masks, [0, 0, 0], [real_num_detection, -1, -1])
+        detection_masks_reframed = utils_ops.reframe_box_masks_to_image_masks(
+            detection_masks, detection_boxes, image.shape[0], image.shape[1])
+        detection_masks_reframed = tf.cast(
+            tf.greater(detection_masks_reframed, 0.5), tf.uint8)
+        tensor_dict['detection_masks'] = tf.expand_dims(
+            detection_masks_reframed, 0)
+      image_tensor = tf.get_default_graph().get_tensor_by_name('image_tensor:0')
+      output_dict = sess.run(tensor_dict,
+                             feed_dict={image_tensor: np.expand_dims(image, 0)})
+
+      output_dict['num_detections'] = int(output_dict['num_detections'][0])
+      output_dict['detection_classes'] = output_dict[
+          'detection_classes'][0].astype(np.uint8)
+      output_dict['detection_boxes'] = output_dict['detection_boxes'][0]
+      output_dict['detection_scores'] = output_dict['detection_scores'][0]
+      if 'detection_masks' in output_dict:
+        output_dict['detection_masks'] = output_dict['detection_masks'][0]
+  return output_dict
+
+
+app = Flask(__name__)
+
+
+@app.route('/',methods=['GET'])
+def hello_world():
+    return render_template('index.html')
+
+@app.route("/predict",methods=['GET','POST'])
+def predictroute():
+    file = request.files['file']
+    image = Image.open(file)
+    IMAGE_SIZE = (12, 8)
+    fnupy = load_image_into_numpy_array(image)
+    image_np_expanded = np.expand_dims(fnupy, axis=0)
+    output = run_inference_for_single_image(fnupy,detection_graph)
+    vis_util.visualize_boxes_and_labels_on_image_array(
+        fnupy,
+        output['detection_boxes'],
+        output['detection_classes'],
+        output['detection_scores'],
+        category_index,
+        instance_masks=output.get('detection_masks'),
+        use_normalized_coordinates=True,
+        line_thickness=8)
+
+    plt.figure(figsize=IMAGE_SIZE)
+    plt.imshow(fnupy)
+    plt.axis('off')
+    new_name = 'pred'+str(time.time()) +'.jpg'
+    # os.remove('{}'.format(new_name))
+    for filename in os.listdir('static/'):
+        if filename.startswith('pred'):  # not to remove other images
+            os.remove('static/' + filename)
+
+    # plt.savefig(new_name)
+    plt.savefig('static/' + new_name)
+    print("{}".format(os.path.join(new_name)))
+    return render_template('result.html',graph=new_name)
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
